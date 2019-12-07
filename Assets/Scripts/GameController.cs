5340664de7c2fca7b9847657acd834d3b316c222
Girali <thegirali@wp.pl>
2019-12-07 02:19:25 +0100

    Player Spawn

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+
+    public Transform spawnPointPlayer;
+    public Transform spawnPointMaster;
+
+    private void Awake()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, spawnPointMaster.position, spawnPointMaster.rotation);
+        }
+        else
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, spawnPointPlayer.position, spawnPointPlayer.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviour
+{
+
+    bool canMove = true;
+    float speed = 5f;
+    float currentLookSensitivity = 2f;
+    float currentCameraRotationX = 0f;
+    float cameraRotationLimit = 85f;
+
+    PhotonView pv;
+    Rigidbody rb;
+    [SerializeField]
+    Transform view = null;
+
+    void Mouvement(float x, float z)
+    {
+        if (canMove)
+        {
+            Vector3 horizontalMove = Vector3.right * x;
+            Vector3 verticalMove = Vector3.forward * z;
+
+            Vector3 move = (horizontalMove + verticalMove).normalized * speed;
+            move.y = rb.velocity.y;
+            transform.Translate(move * Time.deltaTime);
+        }
+    }
+
+    void CameraMouvement(float y, float x)
+    {
+        if (canMove)
+        {
+            float yRot = y;
+            Vector3 rotation = new Vector3(0f, yRot, 0f) * currentLookSensitivity;
+
+            float xRot = x;
+            float cameraRotationX = xRot * currentLookSensitivity;
+
+            rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+
+            currentCameraRotationX -= cameraRotationX;
+            currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+            view.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+        }
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        pv = GetComponent<PhotonView>();
+
+        if (pv.IsMine)
+        { 
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            gameObject.tag = "LocalPlayer";
+
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                for (int i = 0; i < view.childCount; i++)
+                {
+                    view.GetChild(i).GetComponent<Camera>().clearFlags = CameraClearFlags.Skybox;
+                }
+            }
+            else
+            {
+                Destroy(GetComponent<MeshRenderer>());
+                Destroy(GetComponent<MeshFilter>());
+                transform.localScale = new Vector3(1, 50, 1);
+                speed = 25f;
+            }
+        }
+        else
+        {
+            Destroy(view.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (pv.IsMine)
+        {
+
+            CameraMouvement(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (pv.IsMine)
+        {
+
+            Mouvement(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+    }
+}
