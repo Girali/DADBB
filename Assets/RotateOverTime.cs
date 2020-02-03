fec21d7457a4d20f4f7464e630ea97bbbc4bddc9
BerardVictor <victorberardgh@gmail.com>
2020-02-03 17:21:24 +0100

    new asset + rangement + collider

---
+++ Assets/RotateOverTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOverTime : MonoBehaviour
+{
+	public float speed;
+    void Update()
+    {
+        transform.eulerAngles += Vector3.up * Time.deltaTime * speed;
+    }
+}
