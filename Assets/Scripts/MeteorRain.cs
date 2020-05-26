54c7d1ccba6d2accb9fa577e8efcc187d89363f6
Garlockx <Garlockx@users.noreply.github.com>
2020-05-26 15:15:13 +0200

    MeteorRainAnimation
    Add meteor rain animation and script for spawn

---
+++ Assets/Scripts/MeteorRain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorRain : MonoBehaviour
+{
+    private Vector3 meteorRainSpawnPos;
+    public GameObject meteorRain;
+    public float spawnDistance = 10.0f;
+    public float spawnHeight = 3f;
+
+    void SpawnMeteorRain()
+    {
+        meteorRainSpawnPos = Camera.main.transform.position + Camera.main.transform.forward * spawnDistance;
+
+        meteorRainSpawnPos.y += spawnHeight;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            LaunchMeteorRain();
+        }
+    }
+
+    void LaunchMeteorRain()
+    {
+        Instantiate(meteorRain, meteorRainSpawnPos, Quaternion.identity);
+    }
+}
